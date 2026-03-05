f3a5b9b7cb21562038785e00f65628381307720e
Nguyen Thanh Trung <trungnt@savameta.com>
2026-03-05 18:01:28 +0700

    refactor: update namespace

--- Runtime/Core/SerializationService.cs
+++ Runtime/Core/SerializationService.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
-using NekoLib.Core;
+using NekoLib.ColorPalette;
 using NekoLib.Extensions;
 using NekoLib.Logger;
 using NekoLib.Services;
